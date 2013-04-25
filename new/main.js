ed31d4230a5dddd8444cffdfd308a5a0158730b1
Ankit Sardesai <amsardesai@gmail.com>
2013-04-25 01:54:09 -0400

    Responsive web design for below 640px width

--- new/main.js
+++ new/main.js
@@ -21,8 +21,9 @@ $(document).ready(function() {
 	$("#wrapper").addClass("initialHide");
 	$("#arrow").append($("<div>").attr("id","box"));
 	var items = $("#wrapper #header #menuItems li").addClass("animationReset");
-	var d1 = 300, d2 = 100;
-	$("#wrapper").fadeIn(d1, function() {
+
+	$("#wrapper").fadeIn(500, function() {
+		var d1 = 300, d2 = 100;
 	    items.each(function(index) {
 			var delay = d2 * index;
 			var opacity = 0.7; 
@@ -34,7 +35,7 @@ $(document).ready(function() {
 	    $("#arrow #box").delay(items.length*d2).fadeIn(250);
 	});
 	var slider = $("<div>").attr("id","slider");
-	$("#wrapper #container").wrapInner(slider.css({width:3*$("#wrapper .body").outerWidth()}));
+	$("#wrapper #container").wrapInner(slider.css({width:"300%"}));
 	$("#wrapper .body").css({"float":"left"}).attr("id","");
 	$(window).hashchange(refreshPages);
 	$(window).hashchange();
@@ -43,78 +44,22 @@ $(document).ready(function() {
 function changeSlide() {
 	var delay = 250;
 	var items = $("#wrapper .body");
-	var w = items.outerWidth();
+	var w = -100*curSlide + "%";
 	var h = items.eq(curSlide).outerHeight();
-	var m = $("#wrapper #header #menuItems li").outerWidth(true);
-	$("#wrapper #container #slider").animate({"left": -w*curSlide},delay);
-	$("#wrapper #container").animate({"height":h},delay);
-	$("#arrow #box").animate({"left":m*curSlide},delay);
+	var m = (100/3)*curSlide + "%";
+	$("#wrapper #container #slider").animate({left:w},delay);
+	$("#wrapper #container").animate({height:h},delay);
+	$("#arrow #box").animate({left:m},delay);
 }
 
 function refreshPages() {
 	var hash = window.location.hash;
 	if (hash=="#projects") {
 		curSlide = 1;
-		changeSlide();
 	} else if (hash=="#contact") {
 		curSlide = 2;
-		changeSlide();
 	} else {
 		curSlide = 0;
-		changeSlide();
 	}
+	changeSlide();
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-function refreshPages() {
-	var hash = window.location.hash;
-	if (hash=="#projects") {
-		showProjects();		
-	} else if (hash=="#contact") {
-		showContact();
-	} else {
-		showAbout();
-	}
-}
-
-function showAbout() {
-	$("#projects").removeClass("shown").addClass("notshown");
-	$("#contact").removeClass("shown").addClass("notshown");
-	$("#about").removeClass("notshown").addClass("shown");
-	doTheSlide();
-	document.title = "About - Ankit Sardesai";
-}
-
-function showProjects() {
-	$("#about").removeClass("shown").addClass("notshown");
-	$("#contact").removeClass("shown").addClass("notshown");
-	$("#projects").removeClass("notshown").addClass("shown");
-	doTheSlide();
-	document.title = "Projects - Ankit Sardesai";
-}
-
-function showContact() {
-	$("#about").removeClass("shown").addClass("notshown");
-	$("#projects").removeClass("shown").addClass("notshown");
-	$("#contact").removeClass("notshown").addClass("shown");
-	doTheSlide();
-	document.title = "Contact Me - Ankit Sardesai";
-}
-
-function doTheSlide() {
-	var slideDelay = 400;
-	var timeBetweenUpDown = 100;
-	$(".notshown").slideUp(slideDelay).fadeOut(slideDelay);
-	$(".shown").delay(slideDelay + timeBetweenUpDown).slideDown(slideDelay).fadeIn(slideDelay);
-}
-*/
