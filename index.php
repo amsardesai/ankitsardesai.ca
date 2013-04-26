765da7d767dacdbfdfd1a54ee24a8be80011bf6a
Ankit Sardesai <amsardesai@gmail.com>
2013-04-25 22:24:07 -0400

    Created and added favicon to website

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 	<title>Ankit Sardesai</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="base.css" type="text/css" />
 	<!--[if lte IE 8]><link rel="stylesheet" href="iestyles.css" type="text/css" /><![endif]-->
 	<script src="jquery.js" type="text/javascript"></script>
