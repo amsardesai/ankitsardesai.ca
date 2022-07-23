34fb2b29eacedfdf1abd4e09da57f64b2da5a81f
Ankit Sardesai <amsardesai@fb.com>
2022-07-22 17:58:31 -0700

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
 
-  // target: 'web',
-  // cache: true,
-  // context: __dirname,
-  // devtool: 'none',
-  // debug: false,
   entry: './app/client.js',
   mode: 'production',
 
   output: {
     path: join(__dirname, 'build/static'),
     filename: 'bundle.js',
-    // chunkFilename: '[name].[id].js',
     publicPath: '/assets/',
   },
 
@@ -84,48 +78,6 @@ export default {
       }
     }
   },
-
-  // externals: {
-    // 'source-map-support': null,
-  // },
-
-  // plugins: [
-    // new webpack.DefinePlugin({
-      // 'process.env': { NODE_ENV: JSON.stringify('production') },
-      // IS_CLIENT: true,
-      // IS_SERVER: false,
-    // }),
-    //new webpack.optimize.DedupePlugin(),
-    //new webpack.optimize.OccurenceOrderPlugin(),
-    //new webpack.optimize.UglifyJsPlugin(),
-  // ],
-
-  // babel: {
-    // presets: ['es2015', 'react', 'stage-0'],
-    // plugins: ['transform-decorators-legacy'],
-  // },
-
-  // module: {
-    // loaders: [
-      // {
-        // test: /\.jsx?$/,
-        // loaders: ['babel'],
-        // include: [path.resolve(__dirname, 'app')],
-      // },
-    // ],
-  // },
-
-  // node: {
-    // fs: 'empty',
-    // buffer: 'empty',
-    // util: 'empty',
-    // events: 'empty',
-    // assert: 'empty',
-  // },
-
-  // resolve: {
-    // extensions: ['', '.jsx', '.js'],
-  // },
 
 };
 
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,32 +10,6 @@ import config from './config.js';
 
 const webpackConfig = cloneDeep(webpackProdConfig);
 
-
-
-// class WebpackWatchRunPlugin {
-    // constructor(options) {
-        // if (typeof options !== "object") options = {};
-        // this.options = options;
-    // }
-
-    // apply(compiler) {
-        // const options = this.options;
-        // compiler.plugin("watch-run",
-            // function (watching, done) {
-                // const changedTimes = watching.compiler.watchFileSystem.watcher.mtimes;
-                // const changedFiles = Object.keys(changedTimes)
-                    // .map(file => `\n  ${file}`)
-                    // .join("");
-                // if (changedFiles.length) {
-                    // console.log("Files modified:", changedFiles);
-                // }
-                // done();
-            // });
-    // }
-// }
-
-
-
 webpackConfig.mode = 'development';
 webpackConfig.output.publicPath = `http://localhost:${config.ports.webpack}/`;
 webpackConfig.devServer = {
@@ -61,42 +35,4 @@ webpackConfig.module.rules[1].use[1].options.sourceMap = true;
 
 // Make sure we don't clobber our other configuration.
 export default webpackConfig;
-
-// webpackConfig.debug = true;
-// webpackConfig.devtool = 'eval-source-map';
-
-// webpackConfig.plugins = [
-    // new webpack.DefinePlugin({
-      // 'process.env': { NODE_ENV: JSON.stringify('production') },
-      // IS_CLIENT: true,
-      // IS_SERVER: false,
-    // }),
-    // new webpack.HotModuleReplacementPlugin(),
-    // //new webpack.NoErrorsPlugin(),
-// ];
-
-// webpackConfig.devServer = {
-    // publicPath: `http://localhost:${config.ports.webpack}/js/`,
-    // contentBase: path.join(__dirname, config.files.staticAssets),
-    // hot: true,
-    // inline: true,
-    // silent: true,
-    // noInfo: true,
-    // headers: { 'Access-Control-Allow-Origin': '*' },
-    // stats: { colors: true },
-// };
-
-// webpackConfig.output.publicPath =
-  // `http://localhost:${config.ports.webpack}/js/`;
-// webpackConfig.output.hotUpdateMainFilename = 'update/[hash]/update.json';
-// webpackConfig.output.hotUpdateChunkFilename = 'update/[hash]/[id].update.js';
-
-// // Add entry points
-// webpackConfig.entry.unshift(
-  // `webpack-dev-server/client?http://localhost:${config.ports.webpack}`,
-  // 'webpack/hot/only-dev-server'
-// );
-
-// // Modify JS loader so that react-hot works
-// webpackConfig.module.loaders[0].loaders.unshift('react-hot');
 
